abfbed477ec669b88846a23a9854ff4a1c61a360
Irpan Abdul Rahman <irpanar97@gmail.com>
2024-04-16 19:44:00 +0700

    init

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     */
+    public function sendResponse($result, $message) {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * error response method.
+     *
+     */
+    public function sendError($error, $errorMessage = [], $code = 404) {
+        $response = [
+            'success' => false,
+            'message' => $error
+        ];
+
+        if (!empty($errorMessage)) {
+            $response['data'] = $errorMessage;
+        }
+
+        return response()->json($response, $code);
+    }
+}
---
+++ database/migrations/2024_04_15_063224_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('cover', 100);
+            $table->text('content');
+            $table->date('publication_date')->nullable();
+            $table->enum('is_draft', [0, 1])->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
